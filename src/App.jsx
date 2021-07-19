9070e2a9c5a184221038fc01cbc6d28d55c220b5
Rafael Moura <rafaelcostamoura@gmail.com>
2021-07-19 07:02:12 -0300

    Adicionando arquivos de configuraçao do ambiente

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/global.scss'
+
+function App() {
+    return (
+        <div>
+            <h1>Apenas o App2</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
+    mode: isDev ? 'development' : 'production',
+    devtool: isDev ? 'eval-source-map' : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+}
